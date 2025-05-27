e1874fc479fe239e9d1aafbf93dab861aff37d7f
osama-ghazal <semoghazal@gmail.com>
2025-05-27 13:20:25 +0300

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { PageAComponent } from './page-a/page-a.component';
 import { PageBComponent } from './page-b/page-b.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'pageA', component: PageAComponent },
+  // root now loads Page A directly
+  { path: '', component: PageAComponent },
   { path: 'pageB', component: PageBComponent },
   { path: '**', redirectTo: '' }
 ];
