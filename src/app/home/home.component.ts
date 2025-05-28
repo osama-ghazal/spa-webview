ce67700b273a815761c1d193be13b96f9dc22aff
osama-ghazal <semoghazal@gmail.com>
2025-05-28 09:59:54 +0300

    .

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+
+declare global {
+  interface Window { AndroidBridge: { sendMessage(msg: string): void } }
+}
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  imports: [CommonModule, RouterModule]
 })
 export class HomeComponent {
+  showButtons = false;
+  bridgeMessage = '';
+
   constructor(private router: Router) {}
 
-  goToPageA() {
-    this.router.navigate(['/pageA']);
+  toggleButtons() {
+    this.showButtons = !this.showButtons;
+  }
+
+  goToPageB(type: 'spa' | 'bridge') {
+    if (type === 'spa') {
+      this.router.navigate(['/pageB']);
+    } else if (window.AndroidBridge?.sendMessage) {
+      const msg = '/pageB';
+      window.AndroidBridge.sendMessage(msg);
+      this.bridgeMessage = msg;
+    } else {
+      console.warn('AndroidBridge not available');
+    }
   }
 }
